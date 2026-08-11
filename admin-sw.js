65bd7c0d354eb55de9f2ec0e5bc88a426546f73c
h4jq7z68d9-david <davidknkc@icloud.com>
2026-08-11 18:28:06 -0500

    Add files via upload

--- admin-sw.js
+++ admin-sw.js
@@ -1,5 +1,5 @@
-// Admin PWA Service Worker — cache version: dna-admin-v34
-const CACHE = 'dna-admin-v34';
+// Admin PWA Service Worker — cache version: dna-admin-v35
+const CACHE = 'dna-admin-v35';
 const SHELL = [
   '/admin.html',
   '/admin.webmanifest',
